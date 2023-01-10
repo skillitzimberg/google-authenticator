d3aa402c97d148b1f25ae786321d94415eefb72c
Scott Bergler <commandinghands@gmail.com>
2023-01-10 06:03:05 -0800

    Create a basic User datastore & logic for when to add a User to the datastore

--- scripts.js
+++ scripts.js
@@ -2,11 +2,18 @@ const usernameInput = document.getElementById("username");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const confirmInput = document.getElementById("confirm");
+
+const users = [];
+
 document.getElementById("submit").addEventListener("click", function (e) {
   e.preventDefault();
-  var submit = registrationFormIsValid();
-  console.log("SUBMT: ", submit);
-  return submit;
+  if (registrationFormIsValid()) {
+    registerUser({
+      userName: usernameInput.value,
+      email: emailInput.value,
+      password: passwordInput.value,
+    });
+  }
 });
 
 function registrationFormIsValid() {
@@ -15,4 +22,17 @@ function registrationFormIsValid() {
   if (!passwordInput.value) return false;
   if (passwordInput.value != confirmInput.value) return false;
   return true;
+}
+
+function registerUser(user) {
+  if (!userExists(user)) {
+    users.push(user);
+  }
+}
+
+function userExists(user) {
+  const userFound = users.find(
+    (usr) => usr.userName == user.userName || usr.email == user.email
+  );
+  return !!userFound;
 }
