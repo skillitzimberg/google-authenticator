bf74a8f1f326925713bc62a033d72720b63c0ee6
Scott Bergler <commandinghands@gmail.com>
2023-01-23 06:01:01 -0800

    Add event listeners only if the target element is present

--- scripts.js
+++ scripts.js
@@ -4,16 +4,21 @@ const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const confirmInput = document.getElementById("confirm");
 
-document.getElementById("register").addEventListener("click", function (e) {
-  e.preventDefault();
-  if (registrationFormIsValid()) {
-    registerUser({
-      userName: usernameInput.value,
-      email: emailInput.value,
-      password: passwordInput.value,
-    });
-  }
-});
+const registerButton = document.getElementById("register");
+
+if (registerButton) {
+  registerButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (registrationFormIsValid()) {
+      registerUser({
+        userName: usernameInput.value,
+        email: emailInput.value,
+        password: passwordInput.value,
+      });
+    }
+    console.log(users);
+  });
+}
 
 function registrationFormIsValid() {
   if (!usernameInput.value) return false;
@@ -34,4 +39,22 @@ function userExists(user) {
     (usr) => usr.userName == user.userName || usr.email == user.email
   );
   return !!userFound;
+}
+
+const loginButton = document.getElementById("login");
+
+if (loginButton) {
+  loginButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (userIsAuthenticated(usernameInput, passwordInput)) {
+      console.log("YAY! You're logged in!");
+    }
+  });
+}
+
+function userIsAuthenticated(username, password) {
+  const authenticatedUser = users.find(
+    (usr) => usr.username == username && usr.password == password
+  );
+  return !!authenticatedUser;
 }
